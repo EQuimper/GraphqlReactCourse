3bc5450ce6305611f58327633d592ec4b8f14025
EQuimper <quimperemanuel@gmail.com>
2017-02-16 21:51:18 -0500

    Add apolloclient

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Router } from 'react-router';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <div>Lyrical</div>
+    </ApolloProvider>
+  )
 };
 
-ReactDOM.render(
+render(
   <Root />,
   document.querySelector('#root')
 );
