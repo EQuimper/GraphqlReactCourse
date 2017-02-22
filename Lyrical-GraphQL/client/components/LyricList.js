94f6623aeb61103a91e340fb55ea2f84d51a79ec
EQuimper <quimperemanuel@gmail.com>
2017-02-21 20:30:27 -0500

    Add mutation for like lyric

--- Lyrical-GraphQL/client/components/LyricList.js
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import likeLyricMutation from '../queries/likeLyric';
 
 class LyricList extends Component {
+  _toggleLiked(id) {
+    this.props.mutate({
+      variables: {
+        id
+      }
+    });
+  }
+
   _renderLyrics() {
     return this.props.lyrics.map(({ id, content }) => (
       <li key={id} className="collection-item">
         {content}
+        <i
+          className="material-icons"
+          onClick={() => this._toggleLiked(id)}
+        >
+          thumb_up
+        </i>
       </li>
     ));
   }
@@ -17,4 +33,4 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+export default graphql(likeLyricMutation)(LyricList);
